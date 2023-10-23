f7b24e92a9ac0e3bb0c3b4f66ed5ae6cfb2549cc
lekhoa12345678 <lekhoa26061983@gmail.com>
2023-10-23 09:33:37 +0700

    v1

---
+++ src/Component/Header/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function Header() {
+  return (
+   <div>
+    <nav>
+      <a href="/">trang chu</a>
+      <a href="/gioi-thieu">gioi thieu</a>
+      <a href="/post">form</a>
+    </nav>
+   </div>
+  
+  )
+}
+
+export default Header
